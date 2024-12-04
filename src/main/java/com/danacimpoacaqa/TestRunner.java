4c3aafdfc454083a03af537eaa9b3cc6ebb74790
Dana Cimpoaca <dana.cimpoaca@smartsoftdev.eu>
2024-12-04 15:53:08 +0200

    Implement the Driver Manager and configure the Selenium dependencies.

--- src/main/java/com/danacimpoacaqa/TestRunner.java
+++ src/main/java/com/danacimpoacaqa/TestRunner.java
@@ -1,7 +1,31 @@
 package com.danacimpoacaqa;
 
+import com.danacimpoacaqa.managers.DriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+
+import org.openqa.selenium.WindowType;
+
 public class TestRunner {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        WebDriver driver = DriverManager.getInstance().getDriver();
+        driver.get("https://www.google.com/");
+
+        String theNameOfTheFirstTab = driver.getWindowHandle();
+
+        // Open a new window and navigate to DIEZ page
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.get("https://diez.md/");
+        // Close the current tab
+        driver.close();
+
+        driver.switchTo().window(theNameOfTheFirstTab);
+        driver.get("https://www.stiri.md");
+
+
+        driver.quit();
     }
 }
+//DriverManager.getInstance().getDriver().quit();
+
